7e55c801df88dd542babf660fb8f62fc73a31cb9
Trevor <tstike44@hotmail.com>
2019-05-19 14:15:59 -0700

    removed oauth login package

--- server.js
+++ server.js
@@ -15,21 +15,6 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-//login route
-router.use('/api/login',require('react-express-oauth-login-system/routes/loginsystem.js'));
-
-var authenticate = require('react-express-oauth-login-system/authenticate.js')
-
-// An api endpoint that returns a short list of items
-router.get('/api/getList',authenticate, (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    // note that res.user is available after authentication
-    res.json([{items:list,user:res.user}]);
-    console.log('Sent list of items');
-});
-
-
-
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project");
 
